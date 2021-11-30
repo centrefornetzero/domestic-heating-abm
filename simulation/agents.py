6a3f128eb0db55e8ce543fdc992fcf64bba8c692
Steph <82801691+stephanie-chan-cnz@users.noreply.github.com>
2021-11-30 09:01:39 +0000

    adds household properties to associated with renovation budget and associated tests

--- simulation/agents.py
+++ simulation/agents.py
@@ -1,7 +1,12 @@
+import math
 import random
 
 from abm import Agent
 from simulation.constants import (
+    GB_PROPERTY_VALUE_WEIBULL_ALPHA,
+    GB_PROPERTY_VALUE_WEIBULL_BETA,
+    GB_RENOVATION_BUDGET_WEIBULL_ALPHA,
+    GB_RENOVATION_BUDGET_WEIBULL_BETA,
     HEATING_SYSTEM_FUEL,
     HEATING_SYSTEM_LIFETIME_YEARS,
     BuiltForm,
@@ -59,6 +64,63 @@ class Household(Agent):
     @property
     def heating_fuel(self) -> HeatingFuel:
         return HEATING_SYSTEM_FUEL[self.heating_system]
+
+    @staticmethod
+    def get_weibull_percentile_from_value(
+        alpha: float, beta: float, input_value: int
+    ) -> float:
+
+        """
+        :param alpha: alpha (AKA shape) parameter of a Weibull distribution
+        :param beta: beta (AKA scale) parameter of a Weibull distribution
+        :param input_value: an integer value to be expressed as a percentile of the Weibull distribution
+        :return: the percentile (0.0-1.0) of the Weibull distribution that the input_value corresponds to
+        """
+
+        return 1 - math.exp(-((input_value / beta) ** alpha))
+
+    @staticmethod
+    def get_weibull_value_from_percentile(
+        alpha: float, beta: float, percentile: float
+    ) -> int:
+
+        """
+        :param alpha: alpha (AKA shape) parameter of a Weibull distribution
+        :param beta: beta (AKA scale) parameter of a Weibull distribution
+        :param percentile: a percentile (0.0-1.0) to be expressed as a value from the Weibull distribution
+        :return: the value that the percentile of the Weibull distribution corresponds to
+        """
+        epsilon = 0.0000001
+        return beta * (-math.log(1 + epsilon - percentile)) ** (1 / alpha)
+
+    @property
+    def wealth_percentile(self) -> float:
+
+        """
+        :return: the percentile of the household's property value within GB
+        """
+
+        return self.get_weibull_percentile_from_value(
+            GB_PROPERTY_VALUE_WEIBULL_ALPHA,
+            GB_PROPERTY_VALUE_WEIBULL_BETA,
+            self.property_value,
+        )
+
+    @property
+    def renovation_budget(self) -> float:
+        # An amount a house may set aside for work related to home heating and energy efficiency
+        # Expressed as a proportion of their total renovation budget (10%)
+
+        """
+        :return: the estimated renovation budget of a household, derived from the value of a distribution of GB
+        renovation budgets at a household's wealth percentile
+        """
+
+        return 0.1 * self.get_weibull_value_from_percentile(
+            GB_RENOVATION_BUDGET_WEIBULL_ALPHA,
+            GB_RENOVATION_BUDGET_WEIBULL_BETA,
+            self.wealth_percentile,
+        )
 
     def step(self, model):
         pass
