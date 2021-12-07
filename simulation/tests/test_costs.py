81971dd575963f1b70b92abc3c0d452bcbb06c81
Steph <82801691+stephanie-chan-cnz@users.noreply.github.com>
2021-12-07 10:17:00 +0000

    remove redefinition of household_factory() with import from tests.common.py

--- simulation/tests/test_costs.py
+++ simulation/tests/test_costs.py
@@ -1,57 +1,13 @@
-import datetime
 import random
 
 import pytest
 
-from simulation.agents import Household
-from simulation.constants import (
-    BOILERS,
-    HEAT_PUMPS,
-    BuiltForm,
-    ConstructionYearBand,
-    Epc,
-    HeatingSystem,
-    OccupantType,
-    PropertyType,
-)
+from simulation.constants import BOILERS, HEAT_PUMPS, HeatingSystem
 from simulation.costs import (
     get_heating_fuel_costs_net_present_value,
     get_unit_and_install_costs,
 )
-from simulation.model import CnzAgentBasedModel
-
-
-def household_factory(**agent_attributes):
-    default_values = {
-        "location": "Test Location",
-        "property_value": 264_000,
-        "floor_area_sqm": 82,
-        "off_gas_grid": False,
-        "construction_year_band": ConstructionYearBand.BUILT_1919_1944,
-        "property_type": PropertyType.HOUSE,
-        "built_form": BuiltForm.MID_TERRACE,
-        "heating_system": HeatingSystem.BOILER_GAS,
-        "heating_system_install_date": datetime.date(2021, 1, 1),
-        "epc": Epc.D,
-        "potential_epc": Epc.C,
-        "occupant_type": OccupantType.OWNER_OCCUPIER,
-        "is_solid_wall": False,
-        "walls_energy_efficiency": 3,
-        "windows_energy_efficiency": 3,
-        "roof_energy_efficiency": 3,
-        "is_heat_pump_suitable_archetype": True,
-        "is_heat_pump_aware": True,
-    }
-    return Household(**{**default_values, **agent_attributes})
-
-
-def model_factory(**model_attributes):
-    default_values = {
-        "start_datetime": datetime.datetime.now(),
-        "step_interval": datetime.timedelta(minutes=1440),
-        "annual_renovation_rate": 0.05,
-    }
-    return CnzAgentBasedModel(**{**default_values, **model_attributes})
+from simulation.tests.common import household_factory
 
 
 class TestCosts:
