83be9b04cc1e006926745c52c4591fa1f998c798
Steph <82801691+stephanie-chan-cnz@users.noreply.github.com>
2022-01-10 15:13:30 +0000

    fix failing model test, which requires arg override_heat_pump_suitability to be passed as to model.create_household_agents()

--- simulation/tests/test_model.py
+++ simulation/tests/test_model.py
@@ -53,10 +53,12 @@ def test_create_household_agents() -> None:
     )
     heat_pump_awareness = 0.4
     simulation_start_datetime = datetime.datetime.now()
+    override_heat_pump_suitability = False
     household_agents = create_household_agents(
         household_population,
         heat_pump_awareness,
         simulation_start_datetime,
+        override_heat_pump_suitability,
     )
     household = next(household_agents)
 
