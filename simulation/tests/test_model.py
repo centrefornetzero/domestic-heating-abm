23b6d5dded5255c6f5b9f2cee25ad18707aa6e82
Ryan Jenkinson <30845187+RyanJenkinson@users.noreply.github.com>
2022-02-25 12:51:50 +0000

    Scale the number of new builds to the agent population size (#121)
    * Scale the number of new builds to the agent population size
    
    Added:
    * A scaling factor inside the model.new_builds_per_step property that
      scales according to the model.household_count in relation to the
      ENGLAND_WALES_POPULATION. This means that the number of new builds is
      actually scaled to the number of agents we are running
    
    Changed:
    * `ANNUAL_NEW_BUILDS` -> `ENGLAND_WALES_ANNUAL_NEW_BUILDS` so its more
      explicit what the constant represents
    
    Note: This has been tested locally, and is exhibiting the behaviour we
    would expect.
    
    * Fix tests

--- simulation/tests/test_model.py
+++ simulation/tests/test_model.py
@@ -5,6 +5,7 @@ import pytest
 from dateutil.relativedelta import relativedelta
 
 from simulation.constants import (
+    ENGLAND_WALES_HOUSEHOLD_COUNT_2020,
     HEATING_SYSTEM_LIFETIME_YEARS,
     HOUSEHOLDS_PER_HEAT_PUMP_INSTALLER_FLOOR,
     BuiltForm,
@@ -178,6 +179,21 @@ class TestDomesticHeatingABM:
         )
         assert model.new_builds_per_step == 0
 
+    def test_model_new_builds_per_step_is_scaled_by_population_size(self):
+        england_wales_new_builds = 120_000
+        number_of_agents = 10_000
+        model = model_factory(
+            start_datetime=datetime.datetime(2021, 1, 1),
+            step_interval=relativedelta(months=1),  # 1 step = 1 month
+            annual_new_builds={2021: england_wales_new_builds},
+        )
+        model.add_agents([household_factory() for _ in range(number_of_agents)])
+        population_scale_factor = number_of_agents / ENGLAND_WALES_HOUSEHOLD_COUNT_2020
+        expected_new_builds_per_step = int(
+            (england_wales_new_builds / 12) * population_scale_factor
+        )
+        assert model.new_builds_per_step == expected_new_builds_per_step
+
     @pytest.mark.parametrize("year", {2020, 2021, 2022, 2023, 2024})
     def test_model_does_not_install_heat_pumps_in_new_builds_before_2025(self, year):
         model = model_factory(
@@ -186,16 +202,17 @@ class TestDomesticHeatingABM:
         )
         assert model.heat_pump_installation_capacity_per_step_new_builds == 0
 
-    def test_model_includes_new_builds_per_step_post_2025(self):
-        # 120 new builds per year = 10 new builds per month
+    def test_model_installs_heat_pumps_in_new_builds_post_2025(self):
         model = model_factory(
             start_datetime=datetime.datetime(2025, 1, 1),
-            annual_new_builds={2025: 120, 2026: 240},
+            annual_new_builds={2025: 120_000},
         )
-        assert model.new_builds_per_step == 10
-
-        model.current_datetime = datetime.datetime(2026, 1, 1)
-        assert model.new_builds_per_step == 20
+        model.add_agents([household_factory() for _ in range(10_000)])
+        assert model.heat_pump_installation_capacity_per_step_new_builds > 0
+        assert (
+            model.heat_pump_installation_capacity_per_step_new_builds
+            == model.new_builds_per_step
+        )
 
     def test_model_does_not_install_heat_pumps_in_existing_builds_when_too_many_new_builds(
         self,
@@ -204,6 +221,7 @@ class TestDomesticHeatingABM:
             start_datetime=datetime.datetime(2025, 1, 1),
             annual_new_builds={2025: 120_000_000},
         )
+        model.add_agents([household_factory() for _ in range(10_000)])
         assert model.heat_pump_installation_capacity_per_step_new_builds > 0
         assert model.heat_pump_installation_capacity_per_step_existing_builds == 0
         assert not model.has_heat_pump_installation_capacity
@@ -212,7 +230,7 @@ class TestDomesticHeatingABM:
         model = model_factory(
             start_datetime=datetime.datetime(2025, 1, 1),
             heat_pump_installer_count=1_000_000,
-            annual_new_builds={2025: 120},
+            annual_new_builds={2025: 120_000},
         )
         # We calculate the heat pump installers scaled to the number of agents in the model, so make sure there are sufficient agents
         model.add_agents([household_factory() for _ in range(10_000)])
