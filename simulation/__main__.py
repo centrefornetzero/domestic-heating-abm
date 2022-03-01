25ec307f0991c50d5da2b8fc861df455643d77c1
Ryan Jenkinson <ryan.jenkinson@centrefornetzero.org>
2022-03-01 22:43:40 +0000

    Fix typo `BULDS` to `BUILDS`

--- simulation/__main__.py
+++ simulation/__main__.py
@@ -12,7 +12,7 @@ import structlog
 from dateutil.relativedelta import relativedelta
 
 from abm import write_jsonlines
-from simulation.constants import ENGLAND_WALES_ANNUAL_NEW_BULDS, InterventionType
+from simulation.constants import ENGLAND_WALES_ANNUAL_NEW_BUILDS, InterventionType
 from simulation.model import create_and_run_simulation
 
 structlog.configure(
@@ -217,7 +217,7 @@ if __name__ == "__main__":
             args.air_source_heat_pump_price_discount_date,
             args.heat_pump_installer_count,
             args.heat_pump_installer_annual_growth_rate,
-            ENGLAND_WALES_ANNUAL_NEW_BULDS if args.include_new_builds else None,
+            ENGLAND_WALES_ANNUAL_NEW_BUILDS if args.include_new_builds else None,
         )
 
         with smart_open.open(args.history_file, "w") as file:
